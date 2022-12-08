7db6af2c4cd90db8cba8606e23108699255e5b43
Desfl14 <danielescriba.f@outlook.com>
2022-12-07 23:41:24 -0500

    creacion de la app projects

---
+++ projects/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class project(models.Model):
+    
+    
+    class Meta:
+       
+        verbose_name = 'project'
+        verbose_name_plural = 'projects'
